9044f8fac4923a51e56c2b7471a01e74b47ffa84
Toleubay Alikhan <alikhan@powr.io>
2021-09-03 17:24:34 +0600

    Header and devise added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  # To prevent CSRF errors
+  protect_from_forgery with: :exception
+
+  # Before controller will run it should clarify that user is logged in
+  before_action :authenticate_user!
 end
