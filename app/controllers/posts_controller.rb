3ede461665bfce9e1122be5a853eae23175c4346
Toleubay Alikhan <alikhan@powr.io>
2021-09-09 17:44:58 +0600

    Post added

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+  def show
+    begin
+      @post = Post.find(params[:id])
+    rescue
+      render 'layouts/404.html.erb'
+    end
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:body, :user_id))
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+end 
