29c1b53280675fc839d05bf01725b6712ba6f3bb
Toleubay Alikhan <alikhan@powr.io>
2021-09-16 21:25:12 +0600

    Posts works fine now, posts are ready to go:

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,45 @@
 class PostsController < ApplicationController
+
   def show
     begin
-      @post = Post.find(params[:id])
+      @resource = Post.find(params[:id])
     rescue
       render 'layouts/404.html.erb'
     end
   end
 
   def new
-    @post = Post.new
+    @resource = Post.new
   end
 
   def create
-    @post = Post.new(params.require(:post).permit(:title, :body, :user_id))
-    if @post.save
-      redirect_to @post
+    @resource = Post.new(post_params_required)
+    if @resource.save
+      redirect_to @resource
     else
       render :new
     end
   end
+
+  def edit
+    @resource = Post.find(params[:id])
+  end
+
+  def update
+    @resource = Post.find(params[:id])
+    @resource.update(post_params_required)
+    flash.now[:success] = ["Saved!"]
+    render :edit
+  end
+  
+  def destroy
+    Post.find(params[:id]).delete
+    flash.now[:success] = ["You successfully deleted meme."]
+    redirect_to User.find(params[:user_id])
+  end
+
+  private
+    def post_params_required
+      params.require(:post).permit(:title, :body, :user_id)
+    end
 end 
